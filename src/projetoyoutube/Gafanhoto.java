119e82ad8cee256ddba52d2abc13cc45ce9de185
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2022-01-27 10:35:18 -0300

    Primeiro commit

---
+++ src/projetoyoutube/Gafanhoto.java
@@ -0,0 +1,37 @@
+package projetoyoutube;
+public class Gafanhoto extends Pessoa{
+    // Atributos
+    private String login;
+    private int totAssistido; 
+
+    // Construtor
+    public Gafanhoto(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+    
+    // Métodos especiais 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+    
+    // Métodos por sobreposição
+    @Override
+    public String toString() {
+        return "Gafanhoto{" + super.toString() + "\nlogin=" + login + ", totAssistido=" + totAssistido + '}';
+    }
+    
+}
